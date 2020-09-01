19354f467528c56f514753bdc08fb80b9e073102
kayteecannon <kaytee.cannon@me.com>
2020-09-01 11:46:17 -0400

    Adds function and updates clock every second

--- script.js
+++ script.js
@@ -2,25 +2,30 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-var date = new Date();
-console.log(date);
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
-console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+function runTheClock() {
+    // Get current date and time
+    var date = new Date();
+    console.log(date);
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
-// Multiply current hour by 360 degrees in a circle; divide by 12 hours on clock face
-// Then, add current minute position divided by 12 for accuracy
-let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
+    // Multiply current hour by 360 degrees in a circle; divide by 12 hours on clock face
+    // Then, add current minute position divided by 12 for accuracy
+    let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
 
-// Multiply current minute by 360 degrees in a circle; divide by 60 minutes in an hour
-// Then, add current second position divided by 60 for accuracy
-let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
+    // Multiply current minute by 360 degrees in a circle; divide by 60 minutes in an hour
+    // Then, add current second position divided by 60 for accuracy
+    let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
 
-// Multiply current second by 360 degrees in a circle; divide by 60 seconds in one minute
-let secPosition = sec * 360 / 60;
+    // Multiply current second by 360 degrees in a circle; divide by 60 seconds in one minute
+    let secPosition = sec * 360 / 60;
 
-// Transform rotates clock arms to desired degree
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+    // Transform rotates clock arms to desired degree
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+var interval = setInterval(runTheClock, 1000);
